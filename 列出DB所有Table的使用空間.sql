e39e14e97ea7d2eb3e4cf790b6b120bd26b17e01
leisureist <leisureist@hotmail.com>
2020-05-15 09:41:42 +0800

    Delete 列出DB所有Table的使用空間.sql

--- 列出DB所有Table的使用空間.sql
+++
@@ -1,3 +0,0 @@
-declare @a table(name sysname, row integer, recevered varchar(20), data varchar(20),index_size varchar(20),unused varchar(20));
-insert into @a exec sys.sp_MSforeachtable "sys.sp_spaceused '?'";
-select * from @a order by row desc;
